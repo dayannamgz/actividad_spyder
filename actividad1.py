1798f9371269f1bccd2f2142ec9a354945db581e
Dayanna Montes <montesgomezdayanna@gmail.com>
2025-09-06 18:18:19 -0500

    Operacion suma lista

---
+++ actividad1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep  6 17:54:08 2025
+
+@author: SALA-1
+"""
+
+numero_uno = 10
+numero_dos = 5
+
+suma = numero_uno + numero_dos
+print('suma =',suma)
+
+
